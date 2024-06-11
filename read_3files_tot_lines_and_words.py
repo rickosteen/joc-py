a044ee64a58972fe2c0ebe52433489aa81347c8c
Rick Osteen <rickosteen@icloud.com>
2024-06-11 07:18:01 -0400

    was on q & a with Jean for help to finally solve - had one too many loops

--- read_3files_tot_lines_and_words.py
+++ read_3files_tot_lines_and_words.py
@@ -1,17 +1,20 @@
 #read three files test1, text2, text3 dot txt , count number of lines per file recorded in a file counts.txt
 #total the number of lines all together and the number of words per file plus its sum
 def read3_files():
-    count=int(0) 
     file = []
-    tot_words = []
+    tot_lines = int(0)
+    all_words = int(0)
     for i in range(1,4):
         file = open(f"text{i}.txt")
+        count=int(0)
+        tot_words = int(0)
         for line in file:
-            for words in line:
-                tot_words += len(words)
+            #for words in line:
+            words = line.split()
+            tot_words += len(words)
             count +=1
-        print(f"total for text{i}.txt is {count}")
-    count = int(0)
-    
-    
-    read3_files()
+        tot_lines +=count
+        all_words +=tot_words
+        print(f"total lines for text{i}.txt is {count} , {tot_words} total")
+    print(f"TOTAL: {tot_lines} lines,  {all_words} words")
+read3_files()
